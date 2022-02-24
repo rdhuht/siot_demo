b544da4d9d5be82af52033c24014808118052325
rdhuht <1083376784@qq.com>
2022-02-24 17:17:01 +0800

    ini

---
+++ farmer_faculty_humidity.py
@@ -0,0 +1,32 @@
+import siot
+import time
+import random
+
+SERVER = "10.162.242.206"            # MQTT服务器IP
+CLIENT_ID = "humidity_id"                  # 在SIoT上，CLIENT_ID可以留空
+IOT_pubTopic  = 'farmer/tianqiSensors_humidity'  # “topic”为“项目名称/设备名称”
+IOT_UserName ='siot'            # 用户名
+IOT_PassWord ='siot'         # 密码
+
+siot.init(CLIENT_ID, SERVER, user=IOT_UserName, password=IOT_PassWord)
+siot.connect()
+
+humidity = int(input("模拟湿度初始值："))
+scope = int(input("模拟湿度变化幅度设定："))
+time_interval = int(input("发送信息的频率是（秒）:"))
+
+while True:
+    upDown = random.randint(0, 2)
+    if upDown == 1:
+        up = random.randint(1, scope)
+        humidity += up
+    elif upDown == 2:
+        down = random.randint(1, scope)
+        humidity -= down
+    else:
+        pass
+        
+    siot.publish(IOT_pubTopic, humidity)
+    print(f"湿度：{humidity}%")
+    time.sleep(time_interval)  #隔1秒发送一次
+
---
+++ farmer_faculty_temperature.py
@@ -0,0 +1,31 @@
+import siot
+import time
+import random
+
+SERVER = "10.162.242.206"            # MQTT服务器IP
+CLIENT_ID = "temperature_id"                  # 在SIoT上，CLIENT_ID可以留空
+IOT_pubTopic  = 'farmer/tianqiSensors_temperature'  # “topic”为“项目名称/设备名称”
+IOT_UserName ='siot'            # 用户名
+IOT_PassWord ='siot'         # 密码
+
+siot.init(CLIENT_ID, SERVER, user=IOT_UserName, password=IOT_PassWord)
+siot.connect()
+
+temperature = int(input("模拟温度初始值："))
+scope = int(input("模拟温度变化幅度设定："))
+time_interval = int(input("发送信息的频率是（秒）:"))
+
+while True:
+    upDown = random.randint(0, 2)
+    if upDown == 1:
+        up = random.randint(1, scope)
+        temperature += up
+    elif upDown == 2:
+        down = random.randint(1, scope)
+        temperature -= down
+    else:
+        pass
+        
+    siot.publish(IOT_pubTopic, temperature)
+    print(f"温度：{temperature}℃")
+    time.sleep(time_interval)  #隔1秒发送一次
